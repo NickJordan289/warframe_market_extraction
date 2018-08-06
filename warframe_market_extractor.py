d72f94dff7982c547835b650fd90b9b1bb710a21
Nick Jordan <35259816+NickJordan289@users.noreply.github.com>
2018-08-06 13:10:55 +0800

    Updated to be more consistent with style guide

--- warframe_market_extractor.py
+++ warframe_market_extractor.py
@@ -1,21 +1,22 @@
+
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
 import time
 import json
 
-base_url = "https://warframe.market/items/"
-targets = []
+base_url = 'https://warframe.market/items/'
+targets = [] # Items to search for
 
-# File for urls to search
-with open("input.csv", "r") as file:
+# File for items to search
+with open('input.csv', 'r') as file:
     for line in file:
         targets.append(line.lower().rstrip()) # rstrip removes the newline stored in the file
     file.close()
 
-print(str(len(targets)) + " entries")
-start_time = time.time() # start of timer
-for index, target in enumerate(targets): # loop through targets keeping track of index
+print(str(len(targets)) + ' entries')
+start_time = time.time() # Start of stopwatch
+for index, target in enumerate(targets): # Loop through targets keeping track of index
     try:
         # Make a http request to our target passing in our crawlers agent identifier
         target_url = base_url + target
@@ -24,71 +25,65 @@ for index, target in enumerate(targets): # loop through targets keeping track of
         url = urlopen(q)
         html_doc = url.read()
 
-        # if the opening and reading is successful
-        #print("We're in")
-
-        # parse the html with BeautifulSoup
+        # Parse the html with BeautifulSoup
         soup = BeautifulSoup(html_doc, 'html.parser')
-        # find the application-state json from the document and get its contents
-        data = soup.find("script", id="application-state").contents
+        # Find the application-state json from the document and get its contents
+        data = soup.find('script', id='application-state').contents
 
         # File for writing our string json into
         # '[' and ']' on the ends because it wouldn't work without it
         with open('data.json', 'w') as file:
-            file.write("[")
+            file.write('[')
             for a in data:
                 file.write(a)
-            file.write("]")
+            file.write(']')
             file.close()
 
         # File for retrieving json data
-        # reads json dict into data var
+        # Reads json dict into data var
         with open('data.json', 'r') as file:
             data = json.load(file)[0]
             file.close()
 
-        # loop through and store all plat values in arrays
+        # Loop through and store all plat values in arrays
         buy_orders = []
         sell_orders = []
         try:
-            for order in data["payload"]["orders"]: # loop through sell and buy orders 
-                if(order["user"]["status"] == "ingame"): # only go through orders that have online players
-                    if(order["order_type"] == "buy"): # seperate buy orders and sell orders into their own arrays
-                        buy_orders.append(order["platinum"]) # add to buy order storage
+            for order in data['payload']['orders']: # Loop through sell and buy orders
+                if(order['user']['status'] == 'ingame'): # Only go through orders that have online players
+                    if(order['order_type'] == 'buy'): # Seperate buy orders and sell orders into their own arrays
+                        buy_orders.append(order['platinum']) # Add to buy order storage
                     else:
-                        sell_orders.append(order["platinum"]) # add to sell order storage
+                        sell_orders.append(order['platinum']) # Add to sell order storage
         except Exception as e:
-            print(str(index) + ": " + str(e))
+            print(str(index) + ': ' + str(e))
          
-        buy_orders.sort(reverse=True)   # put highest plat value on top
-        sell_orders.sort()              # put lowest plat value on top
-        #print("Buy: " + str(buy_orders[0]))
-        #print("Sell: " + str(sell_orders[0]))
+        buy_orders.sort(reverse=True)   # Put highest plat value on top
+        sell_orders.sort()              # Put lowest plat value on top
 
         # File for plat value outputs
         # Appends results into our output.csv
-        with open("output.csv", "a") as file:
-            file.write(target+",") # item name
-            if(len(buy_orders)>0): # if there are any online buyers
-                file.write(str(buy_orders[0])+",") # highest buy value
-            if(len(sell_orders)>0): #if there are any online sellers
-                file.write(str(sell_orders[0])) # lowest sell value
-            file.write("\n")
+        with open('output.csv', 'a') as file:
+            file.write(target + ',') # Item name
+            if(len(buy_orders) > 0):
+                file.write(str(buy_orders[0]) + ',') # Highest buy value
+            if(len(sell_orders) > 0):
+                file.write(str(sell_orders[0])) # Lowest sell value
+            file.write('\n')
             file.close()
         
-        if(index < len(targets)-1): # if there are more entries
-            time.sleep(3) # dont want to flood them
+        if(index < len(targets) - 1): # If there are more entries to go through
+            time.sleep(3) # Wait for seconds before next search (Feel free to decrease)
 
         # print(url.headers)
     except HTTPError as e:
         if(e.code == 429):
-            print(target + " wants you to slow down")
-            print("Waiting for " + str(e.headers).split()
-                [6] + " seconds before our next request")
-            time.sleep(int(str(e.headers).split()[6]) + 5)  # +5 because why not
+            print(target + ' wants you to slow down')
+            print('Waiting for ' + str(e.headers).split()[6] + ' seconds before our next request')
+            time.sleep(int(str(e.headers).split()[6]) + 3)  # +3 to slowdown after too many requests
         else:
             print(e.code)
 
-# print results of timer
-print("Took " + str((time.time()-start_time)/60) + " minutes")
-print(str((time.time()-start_time)/len(targets)) + " seconds per entry")
+# Print results of stopwatch
+print('Took ' + str((time.time() - start_time) / 60) + ' minutes')
+print(str((time.time() - start_time) / len(targets)) + ' seconds per entry')
